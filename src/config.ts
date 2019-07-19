cce2e92d117d42e369c242781f475c6f4f5953b5
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2019-07-19 11:39:19 +0200

    take config from environment variables

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
 export const GATSBY_HASURA_GRAPHQL_ENDPOINT =
-  "https://graphql.danilospinelli.com/v1/graphql";
+  process.env.GATSBY_HASURA_GRAPHQL_ENDPOINT;
 
-export const GATSBY_BACKEND_ENDPOINT =
-  "https://backend.danilospinelli.com/api/v1";
+export const GATSBY_BACKEND_ENDPOINT = process.env.GATSBY_BACKEND_ENDPOINT;
 
 export const DUMB_IPA_VALUE_FOR_NULL = "da_indicare@x.it";
