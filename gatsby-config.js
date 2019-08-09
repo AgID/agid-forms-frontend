4f0edd1c11549088701a9f567c9c8a4b3c2c9bc2
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2019-08-09 15:09:25 +0200

    added google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,6 +78,28 @@ module.exports = {
         analyzerPort: 3000,
         production: true
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/do-not-track/**"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0
+        // Enables Google Optimize using your container Id
+        // optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+      }
     }
   ]
 };
