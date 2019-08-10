ea96637e774dd90678dcecab2dc8f85458c67209
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2019-08-10 11:02:08 +0200

    adds static pages

---
+++ src/components/StaticLayout.tsx
@@ -0,0 +1,42 @@
+import { graphql, StaticQuery } from "gatsby";
+import * as React from "react";
+import { getMenu, getSiteConfig } from "../graphql/gatsby";
+import Layout from "./Layout";
+
+const StaticLayout = ({
+  children,
+  title
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query LayoutQuery {
+          menu: allConfigYaml(
+            filter: { menu: { elemMatch: { name: { ne: null } } } }
+          ) {
+            ...MenuFragment
+          }
+          siteConfig: allConfigYaml(filter: { title: { ne: null } }) {
+            ...SiteConfigFragment
+          }
+        }
+      `}
+      render={data => {
+        return (
+          <Layout
+            siteConfig={getSiteConfig(data)}
+            menu={getMenu(data)}
+            title={title || ""}
+          >
+            {children}
+          </Layout>
+        );
+      }}
+    />
+  );
+};
+
+export default StaticLayout;
