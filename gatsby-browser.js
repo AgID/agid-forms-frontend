d8f824ad4538202d56b787949c2c103bed1ab34b
Spinelli Danilo <danilo.spinelli@agid.gov.it>
2019-09-11 23:23:01 +0200

    sentry logging

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,9 +5,10 @@
  */
 
 import React from "react";
+import * as Sentry from "@sentry/browser";
+
 import { ApolloProvider } from "react-apollo";
 import { GraphqlClient } from "./src/graphql/client";
-
 import MDXLayout from "./src/components/MDXLayout";
 
 // import { ThemeProvider } from "styled-components";
@@ -15,6 +16,12 @@ import MDXLayout from "./src/components/MDXLayout";
 
 import { setupI18Next } from "./src/utils/i18n";
 setupI18Next();
+
+// forward javascript errors to sentry
+if (process.env.GATSBY_SENTRY_DSN) {
+  Sentry.init({ dsn: process.env.GATSBY_SENTRY_DSN });
+  console.log("setup sentry endpoint: %s", process.env.GATSBY_SENTRY_DSN);
+}
 
 export const wrapRootElement = ({ element }) => (
   // <ThemeProvider theme={theme}>
